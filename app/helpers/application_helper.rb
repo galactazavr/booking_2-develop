478edf6a6c7ef702309880f85633e67760f67f83
Marak0v <marakov-dmitriy@mail.ru>
2026-06-03 21:00:00 +0300

    feat(search): заменить текстовое поле города на выпадающий список из БД

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def all_db_cities
+    @all_db_cities ||= begin
+      cities = (Hotel.active.distinct.pluck(:city) + Property.active.distinct.pluck(:city))
+      cities.map { |c| c.to_s.strip }.uniq.compact.reject(&:blank?).sort
+    end
+  end
 end
